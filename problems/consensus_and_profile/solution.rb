b7706f12f1dd034d6658d4852c368e6736756f08
Matt Chun-Lum <mchunlum@gmail.com>
2014-06-16 00:39:41 -0700

    consensus and profile

---
+++ problems/consensus_and_profile/solution.rb
@@ -0,0 +1,31 @@
+# Problem: Consensus and Profile
+
+require_relative '../../util/fasta'
+
+data = Rosalind::Fasta.import('dataset.txt')
+
+profile = Hash.new { |h, k| h[k] = Array.new(data.values.first.size, 0) }
+
+consensus = ''
+
+data.values.first.size.times do |i|
+  data.values.size.times do |j|
+    profile[data.values[j][i]][i] += 1
+  end
+
+  max = -1
+  max_key = nil
+  %w{A C G T}.each do |j|
+    if profile[j][i] > max
+      max = profile[j][i]
+      max_key = j
+    end
+  end
+  consensus += max_key
+end
+
+puts consensus
+
+%w{A C G T}.each do |k|
+  puts "#{k}: #{profile[k].join(' ')}"
+end
